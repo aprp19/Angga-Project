83c13bebc7a4b248722dc74eab17c9915f7b86cc
Angga Puja Restu Prakasa <35555645+kierskies@users.noreply.github.com>
2018-11-28 14:34:24 +0700

    Add files via upload

---
+++ Player Movement For Endless Run C#.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed;//Kecepatan Player
+	public float jumpForce;//Lompatan Player
+	private Rigidbody2D myRigidbody;//Tubuh Player
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();//Mengambil komponen tubuh player	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Untuk gerakan ke kanan
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        //Jika Mouse Button Kiri di klik Player akan lompat
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+		}
+	}
+}
