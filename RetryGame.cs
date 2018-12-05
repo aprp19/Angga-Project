eff7dbc9b60b9ebb6c60dc84232acbb748774ba4
Angga Puja Restu Prakasa <35555645+kierskies@users.noreply.github.com>
2018-12-05 13:48:49 +0700

    Add files via upload

---
+++ RetryGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryGame : MonoBehaviour {
+
+	public void RetryScene(){
+		SceneManager.LoadScene("UI");
+	}
+}
