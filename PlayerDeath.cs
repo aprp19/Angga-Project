d1119796949d55bba377e80ad9043045c7a1b404
Angga Puja Restu Prakasa <35555645+kierskies@users.noreply.github.com>
2018-12-08 10:11:33 +0700

    Add files via upload

---
+++ PlayerDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+	public GameObject DeathText;
+	public GameObject DeathButton;
+	public GameObject TouchControl;
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Fall"){
+			DeathText.SetActive(true);
+			DeathButton.SetActive(true);
+			TouchControl.SetActive(false);
+		}
+	}
+}
